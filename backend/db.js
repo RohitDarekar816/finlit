3eaef0335c2d7ba320c7f795e428bc44b9759ecc
Rohit Darekar <rohitdarekar816@gmail.com>
2026-01-11 01:22:05 +0530

    fxing

--- backend/db.js
+++ backend/db.js
@@ -103,7 +103,7 @@ async function initializeModules() {
 }
 
 // MongoDB connection
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URI;
 mongoose.connect(url)
   .then(() => {
     console.log('MongoDB connected');
